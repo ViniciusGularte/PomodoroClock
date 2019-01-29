93a3c777aafe1d4b009c6831b01b7826ccb5772f
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2019-01-29 08:43:47 -0200

    add provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import clock from './components/clock/clock';
+
 class App extends Component {
+
   render() {
+    const counter = (state = 0, action) => {
+    switch (action.type) {
+      case 'INCREMENT': return state + 1
+      case 'DECREMENT': return state - 1
+    }
+      return state
+    }
+
+    const store = createStore(counter)
     return (
       <div className="App">
-        <clock/>
+        <Provider store={store}>
+          <clock/>
+        </Provider>
       </div>
     );
   }
--- src/components/clockInterface/clockInterface.js
+++ src/components/clockInterface/clockInterface.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './clockInterface.scss'
 
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+let store = createStore(counter)
 
 const clockInterface = ({counter, increment, decrement}) =>(
   <div className="component-clock-interface">
